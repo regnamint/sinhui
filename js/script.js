2c45fb1161dcf30cc7a37984baf123cd12b81469
NIKLAUS_REG\mhlie <regnamint@gmail.com>
2022-04-02 03:48:26 +0800

    home resume modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
+
     $('#menu-btn').click(function () {
         $('.mobile-navlink').css('display', 'flex');
     })
 
     $('#menu-close').click(function () {
         $('.mobile-navlink').css('display', 'none');
+    })
+
+    $('.resume').click(function() {
+        $('#resume-modal').show();
+    })
+
+    $('.modal-close').click(function() {
+        $('#resume-modal').hide();
     })
 });
